ef8cb1563dae3ec416d813c6943caac0a50fe1c0
KingSid29 <siddhantmittal103@gmail.com>
2026-09-16 17:44:06 +0100

    activity 2

---
+++ activity_2.py
@@ -0,0 +1,21 @@
+a = 5 
+b = 6
+c = 6
+print(not(a == b))
+print(not(b == c))
+x = "Hello"
+y = "World"
+print("____________________")
+if (not(x == y)) :
+    print(f" {x} and {y} are differnet")
+d = 4
+e = 5
+if not ((d == 1)==(e == 5)):
+    print("Hello")
+f = int(input("Enter a number"))
+if not (f%2 == 0) :
+    print("number is odd")
+else:
+    print("number is Even")
+
+
