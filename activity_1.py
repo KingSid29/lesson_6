279ca0da30c0553871f65674c1325853e0c22e23
KingSid29 <siddhantmittal103@gmail.com>
2026-09-16 17:20:47 +0100

    2 nd half of activity 1

--- activity_1.py
+++ activity_1.py
@@ -6,3 +6,16 @@ if a and b and c:
     print("All numbers have boolean numbers is true")
 else:
     print("At least 1 number has boolean number as false")
+
+d = 67 
+e = -67
+f = 0
+if d > 0 or e > 0:
+    print("either of the number is greater then 0")
+else:
+    print("No number is greater then 0")
+
+if e > 0 or f > 0:
+    print("either of the number is greater then 0")
+else:
+    print("none of them is greater then 0")
